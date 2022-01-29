bac2ff04163b3ce92da739f733fe7d6b8e8f7d8a
sushant kumar modak <sushant.modak02@gmail.com>
2022-01-29 14:38:16 +0530

    first commit

--- src/main/java/com/git/TestCommit.java
+++ src/main/java/com/git/TestCommit.java
@@ -3,6 +3,6 @@ package com.git;
 public class TestCommit {
 
 	public static void main(String[] args) {
-		System.out.println("Git Practice Commit");
+		System.out.println("Git Practice Commit first");
 	}
 }
